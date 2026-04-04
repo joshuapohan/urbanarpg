8b51059d548a63c4e99aa67aa10ae2a24f3333b0
joshua <joshuapohan93@gmail.com>
2026-04-04 20:05:33 +0700

    refactor, add idle animation logic

--- dodge/src/entity/adventurer.rs
+++ dodge/src/entity/adventurer.rs
@@ -11,6 +11,8 @@ pub struct Adventurer {
     #[export]
     speed: f32,
 
+    last_velocity: Vector2,
+
     animated_sprite: Option<Gd<AnimatedSprite2D>>,
 
     base: Base<CharacterBody2D>    
@@ -19,19 +21,45 @@ pub struct Adventurer {
 #[godot_api]
 impl Adventurer{
 
-    fn play_animation(&mut self, velocity: Vector2){
+    fn process_movement(&mut self, delta: f64){
+        let input = Input::singleton();
+        let mut velocity = input.get_vector("move_left", "move_right", "move_up", "move_down");
+
+        if velocity.length() > 0.0 {
+            velocity = velocity.normalized() * self.speed;
+            self.last_velocity = velocity;
+        }
+
+        self.process_animation(velocity);
+        let current_pos = self.base().get_position();
+        let new_pos = (current_pos + velocity * delta as f32);
+        if velocity != Vector2::ZERO {
+        }
+        self.base_mut().set_position(new_pos);
+    }
+
+    fn process_animation(&mut self, velocity: Vector2){
+        let (prefix, v) = if velocity == Vector2::ZERO {
+            ("idle".to_string(), self.last_velocity)
+        } else {
+            ("run".to_string(), velocity)
+        };
+        self.play_animation(prefix, v);
+    }
+
+    fn play_animation(&mut self, prefix: String, velocity: Vector2){
         if velocity.length() > 0.0 {
             if velocity.y < 0.0 {
-                self.animated_sprite.as_mut().unwrap().set_animation("run_up");
+                self.animated_sprite.as_mut().unwrap().set_animation(( prefix+ "_up").as_str());
                 self.animated_sprite.as_mut().unwrap().set_flip_h(false);
             } else if velocity.y > 0.0 {
-                self.animated_sprite.as_mut().unwrap().set_animation("run_down");
+                self.animated_sprite.as_mut().unwrap().set_animation((prefix+"_down").as_str());
                 self.animated_sprite.as_mut().unwrap().set_flip_h(false);
             } else if velocity.x > 0.0 {
-                self.animated_sprite.as_mut().unwrap().set_animation("run_right");
+                self.animated_sprite.as_mut().unwrap().set_animation((prefix+"_right").as_str());
                 self.animated_sprite.as_mut().unwrap().set_flip_h(false);
             } else if velocity.x < 0.0 {
-                self.animated_sprite.as_mut().unwrap().set_animation("run_right");
+                self.animated_sprite.as_mut().unwrap().set_animation((prefix+"_right").as_str());
                 self.animated_sprite.as_mut().unwrap().set_flip_h(true);
             }
             
@@ -49,6 +77,7 @@ impl ICharacterBody2D for Adventurer{
             speed:50.0,
             base: base,
             animated_sprite: None,
+            last_velocity: Vector2::RIGHT,
         }
     }
 
@@ -57,13 +86,6 @@ impl ICharacterBody2D for Adventurer{
     }
 
     fn process(&mut self, delta: f64){
-        let input = Input::singleton();
-        let mut velocity = input.get_vector("move_left", "move_right", "move_up", "move_down");
-        velocity = velocity.normalized() * self.speed;
-
-        self.play_animation(velocity);
-        let current_pos = self.base().get_position();
-        let new_pos = (current_pos + velocity * delta as f32);
-        self.base_mut().set_position(new_pos);
+        self.process_movement(delta);
     }    
 }
