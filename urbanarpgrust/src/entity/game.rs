e5bc6fd3f26ac693b675575cd7b402bb43b7b045
joshuapohan <joshuapohan93@gmail.com>
2026-08-25 21:46:36 +0700

    Add item pickup scene (#6)
    * add item pickup scene
    
    * fix
    
    * add first item pickup system

--- urbanarpgrust/src/entity/game.rs
+++ urbanarpgrust/src/entity/game.rs
@@ -129,8 +129,11 @@ impl MainNode{
             }
             self.player = player;
 
-            let health_changed_callable = Callable::from_object_method(&self.hud.as_ref().unwrap(), "update_health");
-            self.player.as_mut().unwrap().connect("s_health_changes", &health_changed_callable);
+            let player_health_changed_callable = Callable::from_object_method(&self.hud.as_ref().unwrap(), "update_health");
+            self.player.as_mut().unwrap().connect("s_health_changes", &player_health_changed_callable);
+
+            let player_dialogue_ended_callable = Callable::from_object_method(&self.player.as_ref().unwrap(), "on_dialogue_ended");
+            DialogueSystem::singleton().connect("s_dialogue_ended", &player_dialogue_ended_callable);
             
         }
     }
@@ -165,36 +168,6 @@ impl INode2D for MainNode{
             dialogue_box: None,
         }
     }
-    /* 
-    fn input(&mut self, event: Gd<InputEvent>){
-        if event.is_action_pressed("pause"){
-            let is_paused = gamestate::GameState::singleton().bind().is_gameplay_paused();
-            if is_paused {
-                gamestate::GameState::singleton().bind_mut().resume_gameplay();
-            } else {
-                gamestate::GameState::singleton().bind_mut().pause_gameplay();
-            }
-        } else if gamestate::GameState::singleton().bind().is_in_dialogue() {
-            if event.is_action_pressed("interact") {
-                // continue to next line
-                log_info!("In dialogue, advancing");
-                DialogueSystem::singleton().bind_mut().advance();                
-                //DialogueSystem::singleton().bind_mut().next_line();
-                self.base_mut().get_viewport().unwrap().set_input_as_handled();
-            } else if event.is_action_pressed("move_up"){
-                log_info!("In dialogue, up");
-                DialogueSystem::singleton().bind_mut().move_choice_up();
-            } else if event.is_action_pressed("move_down"){
-                log_info!("In dialogue, down");
-                DialogueSystem::singleton().bind_mut().move_choice_down();
-            }
-        } else {
-            if event.is_action_pressed("interact") {
-                self.player.as_mut().unwrap().bind_mut().try_interact();
-            }
-        }        
-    }
-    */
 
     fn physics_process(&mut self, _delta: f64){
         if Input::singleton().is_action_just_pressed("pause"){
